553e6908f2d40f67e6cc017485fc1fa67c15d5d9
Lesley Rossouw <lesley.rossouw@unsw.edu.au>
2026-06-04 11:40:29 +1000

    Working compile package for buildroot
    Signed-off-by: Lesley Rossouw <lesley.rossouw@unsw.edu.au>

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# build_linux_netbench
+# ipbench2.buildroot
 
-This is a script including buildroot packages for TS's Linux network benchmarking demands paired with board defconfigs for popular targets including the packages. 
+This repository contains a [Buildroot](https://buildroot.org) package for [ipbench2](https://github.com/au-ts/ipbench/tree/main/ipbench2). This will allow cross-compiling ipbench2 alongside an arbitrary Linux target.
+
+## Usage
+
+In order to use this package, it must be simlinked into `/path/to/buildroot/package/` and an entry must be added to `/path/to/buildroot/package/Config.in` declaring it to the Buildroot configurator. Here is an example Config.in excerpt, placed in the `Networking Applications` section:
+
+```
+menu "Networking applications"
+    # (place anywhere in this section)
+	source "package/ipbench2/Config.in"
+```
+
+Note: the `menu` the ipbench2 definition is placed in only affects where it is placed in the Buildroot `menuconfig`.
--- ipbench/ipbench2.mk
+++ ipbench/ipbench2.mk
@@ -1,11 +1,13 @@
 # ipbench2 for Buildroot
+
 IPBENCH2_VERSION = 2.1.1
 IPBENCH2_SITE = $(call github,au-ts,ipbench,crosscompilation)
 IPBENCH2_SUBDIR = ipbench2
 IPBENCH2_INSTALL_STAGING = YES
 IPBENCH2_LICENSE = GPL-2.0
 IPBENCH2_LICENSE_FILES = COPYING
-IPBENCH2_DEPENDENCIES = host-swig host-python3 host-libtool
+
+IPBENCH2_DEPENDENCIES = host-swig host-python3 host-libtool python3
 
 # Override autotools configure variables
 IPBENCH2_CONF_ENV = \
@@ -21,36 +23,26 @@ IPBENCH2_CONF_ENV = \
     PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
     PKG_CONFIG_LIBDIR="$(STAGING_DIR)/usr/lib/pkgconfig"
 
+# The below step puts everything in an `/ipbench` subdirectory to avoid
+# libtool freaking out about the staging directory being an unsafe location
+# (detects usr, etc as the ones on your host!). These are moved out again in
+# the POST_INSTALL_TARGET_CMDS.
 define IPBENCH2_CONFIGURE_CMDS
     cd $(@D)/$(IPBENCH2_SUBDIR) && \
     $(TARGET_MAKE_ENV) ./autogen.sh && \
     $(TARGET_CONFIGURE_OPTS) \
-    $(IPBENCH2_CONF_ENV) \
-    ac_cv_path_CC=$(TARGET_CC) \
+    $(IPBENCH2_CONF_ENV) && \
+    mkdir -p $(STAGING_DIR)/ipbench && \
     ./configure \
         --build=$(GNU_HOST_NAME) \
         --host=$(GNU_TARGET_NAME) \
-        --prefix=/usr \
-        --sysconfdir=/etc \
-        --localstatedir=/var \
-        --libdir=/usr/lib \
+        --prefix=/ipbench/usr \
+        --sysconfdir=/ipbench/etc \
+        --localstatedir=/ipbench/var \
+        --libdir=/ipbench/usr/lib \
         --disable-static \
-        --enable-shared \
+        --enable-shared
         --with-sysroot=$(STAGING_DIR)
-endef
-
-# Fix libtool to handle cross-compilation paths
-define IPBENCH2_POST_CONFIGURE_CMDS
-    $(SED) "s,^CC=\"\$$CC\",CC=\"$(TARGET_CC)\"," $(@D)/$(IPBENCH2_SUBDIR)/libtool
-    $(SED) "s,^LD=\"\$$LD\",LD=\"$(TARGET_LD)\"," $(@D)/$(IPBENCH2_SUBDIR)/libtool
-    $(SED) "s,^NM=\"/usr/bin/nm -B\",NM=\"$(TARGET_NM)\"," $(@D)/$(IPBENCH2_SUBDIR)/libtool
-    $(SED) "s,-L/usr/lib[^[:space:]]*,,g" $(@D)/$(IPBENCH2_SUBDIR)/libtool
-    $(SED) "s,-L/lib[^[:space:]]*,,g" $(@D)/$(IPBENCH2_SUBDIR)/libtool
-    $(SED) "s,\$${wl}-rpath \$${wl}/usr/lib,,g" $(@D)/$(IPBENCH2_SUBDIR)/libtool
-    $(SED) 's,^runpath_var=.*,runpath_var=,' $(@D)/$(IPBENCH2_SUBDIR)/libtool
-    $(SED) 's,^sys_lib_search_path_spec=.*,sys_lib_search_path_spec="$(STAGING_DIR)/usr/lib",' $(@D)/$(IPBENCH2_SUBDIR)/libtool
-    $(SED) 's,^sys_lib_dlsearch_path_spec=.*,sys_lib_dlsearch_path_spec="$(STAGING_DIR)/usr/lib",' $(@D)/$(IPBENCH2_SUBDIR)/libtool
-    $(SED) "s,-L/usr/lib,-L$(STAGING_DIR)/usr/lib,g" $(@D)/$(IPBENCH2_SUBDIR)/libtool
 endef
 
 define IPBENCH2_BUILD_CMDS
@@ -67,10 +59,41 @@ define IPBENCH2_INSTALL_TARGET_CMDS
         -C $(@D)/$(IPBENCH2_SUBDIR) install
 endef
 
+# Move everything out of the dummy ipbench folder!
+define IPBENCH2_POST_INSTALL_TARGET_CMDS
+    @echo "Moving ipbench2 info to target directory"
+    test -d $(TARGET_DIR)/ipbench/usr || (echo "Error: $(TARGET_DIR)/ipbench/usr does not exist" && exit 1)
+    ls -la $(TARGET_DIR)/ipbench/usr
+    mkdir -p $(TARGET_DIR)/usr
+    cd $(TARGET_DIR)/ipbench/usr && \
+        for d in ./*; do \
+            if [ -d "$$d" ]; then \
+                cp -rv "$$d" $(TARGET_DIR)/usr/ ; \
+            else \
+                cp -v "$$d" $(TARGET_DIR)/usr/ ; \
+            fi \
+        done
+    @echo "Cleaning up temporary directory..."
+    rm -rf $(TARGET_DIR)/ipbench
+endef
+
+IPBENCH2_POST_INSTALL_TARGET_HOOKS += IPBENCH2_POST_INSTALL_TARGET_CMDS
+
 define IPBENCH2_INSTALL_STAGING_CMDS
     $(TARGET_MAKE_ENV) $(MAKE) \
         DESTDIR=$(STAGING_DIR) \
         -C $(@D)/$(IPBENCH2_SUBDIR) install
+endef
+
+# Fix libtool to handle cross-compilation
+define IPBENCH2_POST_CONFIGURE_CMDS
+    $(SED) "s,^CC=\"$$CC\",CC=\"$(TARGET_CC)\"," $(@D)/$(IPBENCH2_SUBDIR)/libtool
+    $(SED) "s,^LD=\"$$LD\",LD=\"$(TARGET_LD)\"," $(@D)/$(IPBENCH2_SUBDIR)/libtool
+    $(SED) "s,^NM=\"/usr/bin/nm -B\",NM=\"$(TARGET_NM)\"," $(@D)/$(IPBENCH2_SUBDIR)/libtool
+    $(SED) "s,-L/usr/lib,,g" $(@D)/$(IPBENCH2_SUBDIR)/libtool
+    $(SED) "s,\$${wl}-rpath \$${wl}/usr/lib,,g" $(@D)/$(IPBENCH2_SUBDIR)/libtool
+    $(SED) 's,^sys_lib_search_path_spec=.*,sys_lib_search_path_spec="$(STAGING_DIR)/usr/lib",' $(@D)/$(IPBENCH2_SUBDIR)/libtool
+    $(SED) 's,^sys_lib_dlsearch_path_spec=.*,sys_lib_dlsearch_path_spec="$(STAGING_DIR)/usr/lib",' $(@D)/$(IPBENCH2_SUBDIR)/libtool
 endef
 
 $(eval $(autotools-package))
